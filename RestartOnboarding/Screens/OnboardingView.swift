b99218586c61dba0a21af40b09f609f7c2d6508e
Shubham Kumar <kumar.shubham1329@gmail.com>
2022-05-07 16:30:54 +0530

    All Onboarding Animations are added

--- RestartOnboarding/Screens/OnboardingView.swift
+++ RestartOnboarding/Screens/OnboardingView.swift
@@ -13,6 +13,9 @@ struct OnboardingView: View {
     @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
     @State private var buttonOffset: CGFloat = 0
     @State private var isAnimating: Bool = false
+    @State private var imageOffset: CGSize = .zero
+    @State private var indicatorOpacity: Double = 1.0
+    @State private var textTitle: String = "Share"
     //MARK: - BODY
     var body: some View {
         ZStack {
@@ -22,12 +25,14 @@ struct OnboardingView: View {
                 //MARK: Header
                 Spacer()
                 VStack(spacing: 0) {
-                    Text("Share.")
+                    Text(textTitle)
                         .font(.system(size: 60))
                         .fontWeight(.heavy)
                         .foregroundColor(.white)
                         .multilineTextAlignment(.center)
                         .padding(.horizontal, 10)
+                        .transition(.opacity)
+                        .id(textTitle)
                     
                     Text("""
                         It's not how much we give,
@@ -46,13 +51,46 @@ struct OnboardingView: View {
                 //MARK: Center
                 ZStack {
                     CircleGroupView(shapeColor: .white, shapeOpacity: 0.2)
+                        .offset(x: imageOffset.width * -1)
+                        .blur(radius: abs(imageOffset.width / 5))
+                        .animation(.easeOut(duration: 1), value: imageOffset)
                     Image("character-1")
                         .resizable()
                         .scaledToFit()
                         .opacity(isAnimating ? 1: 0)
                         .animation(.easeOut(duration: 1.5), value: isAnimating)
+                        .offset(x: imageOffset.width * 1.2, y: 0)
+                        .rotationEffect(.degrees(Double(imageOffset.width / 20)))
+                        .gesture(DragGesture()
+                            .onChanged({ gesture in
+                                if abs(imageOffset.width) <= 150 {
+                                    imageOffset = gesture.translation
+                                    withAnimation(.linear(duration: 0.25)) {
+                                        indicatorOpacity = 0
+                                        textTitle = "Give"
+                                    }
+                                }
+                            })
+                                .onEnded({ _ in
+                                    imageOffset = .zero
+                                    withAnimation(.linear(duration: 0.25)) {
+                                        indicatorOpacity = 1
+                                        textTitle = "Share"
+                                    }
+                                })
+                        )
+                        .animation(.easeInOut(duration: 1), value: isAnimating)
                 }//:Center ZStack
-                .animation(.spring(), value: isAnimating)
+                .animation(.spring(), value: imageOffset)
+                .overlay(
+                    Image(systemName: "arrow.left.and.right.circle")
+                        .font(.system(size: 44, weight: .light))
+                        .foregroundColor(.white)
+                        .offset(y: 20)
+                        .opacity(isAnimating ? 1 : 0)
+                        .animation(.easeInOut(duration: 1).delay(2), value: isAnimating)
+                        .opacity(indicatorOpacity),
+                    alignment: .bottom)
                 Spacer()
                 
                 //MARK: Footer
